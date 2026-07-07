3aaca1223d6e29bf7a0bbc6b7a1d58e355105aee
nikhil74589 <nikhilranjan2501@gmail.com>
2026-07-07 16:32:56 +0530

    Smoke Pipeline

--- smoke_test.py
+++ smoke_test.py
@@ -25,13 +25,14 @@ if missing:
 
 # DB FIND banking_db and show tables
 try:
-    connection=pymysql.connect(
-        host=params["DB_HOST"],
-        user=params["DB_USER"],
-        password=params["DB_PASSWORD"],
-        port=int(params["DB_PORT"]),
-        connect_timeout=10
-    )
+    connection = pymysql.connect(
+    host=params["DB_HOST"],
+    user=params["DB_USER"],
+    password=params["DB_PASSWORD"],
+    database=params["DB_NAME"],
+    port=int(params["DB_PORT"]),
+    connect_timeout=10
+)
 
     cur=connection.cursor()
     cur.execute("SHOW TABLES")
